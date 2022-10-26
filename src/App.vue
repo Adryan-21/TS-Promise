e21dfa27fdec4bc17372a8a1073bf7db970ec5ab
Adryan <adrianrogowski80@gmail.com>
2022-10-26 20:36:29 +0200

    .

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,88 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <table>
+    <caption>
+      Pokemons
+    </caption>
+    <thead>
+      <tr>
+        <th>Name</th>
+        <th>Types</th>
+        <th>URL</th>
+        <th>Ability (name)</th>
+        <th>Ability (desc)</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="(pokemon, index) in state.ListPokemons" :key="index">
+        <td>
+          {{ pokemon.name }}
+        </td>
+        <td>
+          {{ pokemon.types }}
+        </td>
+        <td>
+          {{ pokemon.url }}
+        </td>
+        <td>
+          {{ pokemon.ability.name }}
+        </td>
+        <td>
+          {{ pokemon.ability.desc }}
+        </td>
+      </tr>
+    </tbody>
+  </table>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import { defineComponent, reactive } from "vue";
+
+interface Pokemon {
+  name: string;
+  url: string;
+  types: string;
+  ability: {
+    name: string;
+    desc: string;
+  };
+}
 
 export default defineComponent({
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: "App",
+  components: {},
+  setup() {
+    const state = reactive({
+      ListPokemons: [] as Pokemon[],
+    });
+
+    function getData() {
+      fetch("https://pokeapi.co/api/v2/pokemon")
+        .then((res) => res.json())
+        .then((data) => {
+          data.results.forEach((data: Pokemon) => {
+            const pokemon: Pokemon = {} as Pokemon;
+            pokemon.name = data.name;
+            pokemon.url = data.url;
+            fetch(pokemon.url)
+              .then((res) => res.json())
+              .then((data) => {
+                pokemon.types = data.types[0].type.name;
+                pokemon.ability = { name: data.abilities[0].ability.name, desc: "" };
+                fetch(data.abilities[0].ability.url)
+                  .then((res) => res.json())
+                  .then((data) => {
+                    pokemon.ability.desc = data.effect_entries[0].effect;
+                    state.ListPokemons.push(pokemon);
+                  });
+              });
+          });
+        });
+    }
+
+    getData();
+
+    return { state };
+  },
 });
 </script>
 
@@ -20,8 +91,15 @@ export default defineComponent({
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+table,
+td {
+  border: 2px solid black;
+}
+td {
+  padding: 10px;
 }
 </style>
